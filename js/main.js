1eeef625b9eb0cd8279a59997a4ad1282fcdf544
klekovvlad <100075182+klekovvlad@users.noreply.github.com>
2022-09-13 00:15:21 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('.feedback__slider').slick({
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '0px',
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
